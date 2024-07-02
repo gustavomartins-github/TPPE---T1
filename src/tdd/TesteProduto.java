9385220204a87d996136ef91ede01b2ca1547b16
PhilipeSousa <barrosphilipe07@gmail.com>
2024-07-02 00:46:33 -0300

    update teste produto

--- src/tdd/TesteProduto.java
+++ src/tdd/TesteProduto.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-
 import app.Produto;
 
 import java.util.Arrays;
